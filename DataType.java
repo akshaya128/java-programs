d766db3a237138a0e83ae818d6e127ac775665ae
akshaya128 <116068765+akshaya128@users.noreply.github.com>
2022-10-18 15:51:15 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,38 @@
+package ak;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intval;
+		float fval;
+		double dval;
+		long lval;
+		byte bval;
+		char cval;
+		short sval;
+		boolean boolval;
+		
+		//assign values
+		
+		intval = 189;
+		System.out.println("Integer value="+intval);
+		fval = 143.4f;
+		System.out.println("Float value="+fval);
+		dval = 875.4;
+		System.out.println("Double value="+dval);
+		cval='A';
+		System.out.println("character value="+cval);
+		lval=12345;
+		System.out.println("long value="+lval);
+		boolval=true;
+		System.out.println("boolean value="+boolval);
+		sval =10;
+		System.out.println("short value="+sval);
+		bval=15;
+		System.out.println("byte value="+bval);
+		
+
+	}
+
+}
