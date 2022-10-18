b97f35b68898279d4882a737d8c2d878aab968ea
akshaya128 <116068765+akshaya128@users.noreply.github.com>
2022-10-18 14:48:38 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,11 @@
+package la;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm AKSHAYA L");
+		System.out.println("I'm from Tamilnadu");
+		System.out.println("I have completed my Under Gradution in computer science engineering");
+	}
+}
