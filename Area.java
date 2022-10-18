f56796170928ab8714bc5ad791ec5e0a54526744
L.AKSHAYA <116068765+akshaya128@users.noreply.github.com>
2022-10-18 19:30:34 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,28 @@
+package ak;
+
+public class Area {
+
+	public static void main(String[] args) {
+				
+		
+	    // Area of Rectangle
+		  int length=4,width=5;
+	      int rect=length*width;
+		  System.out.println("The area of rectangle of length="+length+" and width ="+width+" is = "+rect);
+		// Area of Square
+		  int a=22;
+          int square=a*a;
+          System.out.println("Area of the square with length "+a+" is "+square);
+	    // Area of Triangle
+	      int b=8,h=5;
+	      int tri=(b*h)/2;
+	      System.out.println("The area of triangle of breadth="+b+" and height="+h+" is = "+tri);
+	    // Area of circle
+	      int r=4;
+	      double pi=3.14;
+	      double circle=pi*r*r;
+	      System.out.println("The area of circle with radius "+r+" is = "+circle);
+
+	}
+}
+
